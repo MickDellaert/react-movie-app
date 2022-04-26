c83247653d7c36e5befe41f281364f636e89ddae
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-04-26 22:13:11 +0200

    filter data from input

--- src/App.js
+++ src/App.js
@@ -15,16 +15,24 @@ function App() {
   const getInput = (getInput) => {
     setInput(getInput.target.value);
 
-  }
-
-
+    if (input.length > 2) {
+      const filteredMovies = data.filter((title) =>
+        title.Title.toLowerCase().includes(input.toLowerCase())
+      );
+      setMovies(filteredMovies);
+      console.log(movies);
+    } else {
+      setMovies(data);
+    }
+    console.log(input.length);
+  };
 
   return (
     <div className="App">
       <h1>React Movie App</h1>
       <p>{input}</p>
-      <MovieInput getInput={getInput}/>
-      <MovieCard data={movies}/>
+      <MovieInput getInput={getInput} />
+      <MovieCard data={movies} />
     </div>
   );
 }
