1c7c3f6eeea860b38e6ca62106a149097260f0ee
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-04-27 02:00:39 +0200

    improved search input styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,10 @@
 const MovieCard = ({ data }) => {
-  return data.map((titles, index) => (
-    <div className="d-flex my-3 px-3 col-sm-12 col-md-4 col-lg-3 col-xl-2">
-      <img
-        className="img-fluid w-100"
-        key={index}
-        src={titles.Poster}
-        alt={titles.Title}
-      />
+  return data.map((titles) => (
+    <div
+      key={titles.Poster}
+      className="d-flex my-2 px-2 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
+    >
+      <img className="img-fluid w-100" src={titles.Poster} alt={titles.Title} />
     </div>
   ));
 };
--- src/components/MovieInput.jsx
+++ src/components/MovieInput.jsx
@@ -1,8 +1,31 @@
 const MovieInput = ({ getInput }) => {
   return (
-    <div className="col d-flex justify-content-end">
-      <input type="text" onChange={(event) => getInput(event)} />
-    </div>
+    <>
+      {/* <div className="col d-flex justify-content-end">
+        <input
+          className="input"
+          type="text"
+          onChange={(event) => getInput(event)}
+        />
+      </div> */}
+      <div className="input-group input">
+        <input
+          className="form-control border-end-0 border"
+          type="search"
+          placeholder="search"
+          id="example-search-input"
+          onChange={(event) => getInput(event)}
+        />
+        <span className="input-group-append">
+          <button
+            className="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border ms-n5"
+            type="button"
+          >
+            <i className="fa fa-search"></i>
+          </button>
+        </span>
+      </div>
+    </>
   );
 };
 
